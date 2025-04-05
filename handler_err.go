0c9d5ff6394ab01cd2b0d1e2e951682721e779c2
Derek Fodeke-Rodgers <Dmonarch134@gmail.com>
2025-04-04 23:15:03 -0500

    update

--- handler_err.go
+++ handler_err.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func handlerErr(w http.ResponseWriter, r *http.Request) {
 	respondWithError(w, 400, "Something went wrong")
+	log.Printf("Received request: %s to %s from %s", r.Method, r.RequestURI, r.RemoteAddr)
+
 }
