8e86d678183672aa9de6b3c0fc121e792e75b0b5
Derek Fodeke-Rodgers <Dmonarch134@gmail.com>
2025-04-06 14:43:19 -0500

    feat: Implement feed management with create feed functionality and middleware authentication

--- handler_feed.go
+++ handler_feed.go
@@ -11,10 +11,11 @@ import (
 	"github.com/google/uuid"
 )
 
-func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Request, user database.User) {
 
 	type parameters struct {
 		Name string `json:"name"`
+		Url  string `json:"url"`
 	}
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
@@ -24,24 +25,21 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		respondWithError(w, 400, fmt.Sprintf("Error Parsing JSON:%v", err))
 		return
 	}
-	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+	feed, err := apiCfg.DB.CreateFeed(r.Context(), database.CreateFeedParams{
 		ID:        uuid.New(),
 		CreatedAt: time.Now().UTC(),
 		UpdatedAt: time.Now().UTC(),
 		Name:      params.Name,
+		Url:       params.Url,
+		UserID:    user.ID,
 	})
 	if err != nil {
-		respondWithError(w, 400, fmt.Sprintf("Unable to create USER:%v", err))
+		respondWithError(w, 400, fmt.Sprintf("Unable to create feed:%v", err))
 
 	}
 
-	respondWithJSON(w, 200, databaseUserToUser(user))
+	respondWithJSON(w, 200, databaseFeedToFeed(feed))
 
 }
 
 //getAPIKey(r.Header)
-
-func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {
-
-	respondWithJSON(w, 200, databaseUserToUser(user))
-}
